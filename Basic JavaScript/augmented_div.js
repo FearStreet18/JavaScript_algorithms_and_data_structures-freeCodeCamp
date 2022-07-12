da2477473ba214698e341f783c6b3633bb7ef2f0
Saadman Sakib <saadman.sakib.ss@gmail.com>
2022-07-13 01:03:35 +0600

    https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/

---
+++ Basic JavaScript/augmented_div.js
@@ -0,0 +1,14 @@
+// Waiting: a should equal 4.
+// Waiting: b should equal 27.
+// Waiting: c should equal 3.
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a/=12;
+b/=4;
+c/=11;
+
+console.log(a,b,c); 
---
+++ Basic JavaScript/augmented_mult.js
@@ -0,0 +1,13 @@
+// a should equal 25.
+// Waiting: b should equal 36.
+// Waiting: c should equal 46.
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a*=5;
+b*=3;
+c*=10;
+
+console.log(a,b,c); 
